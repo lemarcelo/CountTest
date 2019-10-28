31603b6963cf2287b722b15d9f1db6684a8367dd
Marcelo Pereira Santos <lemarcelo@outlook.com>
2019-10-28 17:30:04 -0300

    Add project files.

---
+++ CountSample/CountSample/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CountSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int lblValue = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Increment(object sender, EventArgs e)
+        {
+            lblValue++;
+            lblCount.Text = lblValue.ToString();
+        }
+    }
+}
